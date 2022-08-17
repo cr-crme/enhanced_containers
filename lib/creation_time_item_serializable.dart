b56ee8a8a398c88710495c0f3663f0d9c5acb771
Pariterre <pariterre@hotmail.com>
2022-08-17 13:06:30 -0400

    Finalized TimedList

--- lib/creation_time_item_serializable.dart
+++ lib/creation_time_item_serializable.dart
@@ -3,15 +3,15 @@ import './item_serializable.dart';
 /// The base class for the timable items contained in all the custom containers.
 ///
 /// Written by: @pariterre and @Guibi1
-abstract class TimedItemSerializable extends ItemSerializable {
-  /// Creates an [TimedItemSerializable] with the original [creationTimeStamp].
-  TimedItemSerializable({String? id, int? creationTimeStamp})
+abstract class CreationTimeItemSerializable extends ItemSerializable {
+  /// Creates an [CreationTimeItemSerializable] with the original [creationTimeStamp].
+  CreationTimeItemSerializable({String? id, int? creationTimeStamp})
       : creationTimeStamp =
             creationTimeStamp ?? DateTime.now().microsecondsSinceEpoch,
         super(id: id);
 
-  /// Creates an [TimedItemSerializable] from a map of serialized items.
-  TimedItemSerializable.fromSerialized(map)
+  /// Creates an [CreationTimeItemSerializable] from a map of serialized items.
+  CreationTimeItemSerializable.fromSerialized(map)
       : creationTimeStamp =
             map['creationTimeStamp'] ?? DateTime.now().microsecondsSinceEpoch,
         super.fromSerialized(map);
