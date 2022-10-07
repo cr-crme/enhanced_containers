99e9a9d0a166b96dc4aca922a99e8b96632fbc33
Laurent Stéphenne <guibi.ss999@gmail.com>
2022-10-07 16:22:38 -0400

    Moved code that is specific to Firebase

--- lib/database_list_provided.dart
+++ lib/database_list_provided.dart
@@ -3,26 +3,12 @@ import 'list_provided.dart';
 
 abstract class DatabaseListProvided<T> extends ListProvided<T> {
   /// Creates an empty [DatabaseListProvided].
-  DatabaseListProvided({
-    required this.pathToData,
-    String? pathToAvailableDataIds,
-  }) : _pathToAvailableDataIds = pathToAvailableDataIds ?? '$pathToData-ids';
+  DatabaseListProvided();
 
-  /// You can't use this function with [ListFirebase], as it already saves its content in the cloud automagically.
+  /// You can't use this function with [DatabaseListProvided], as it already saves its content in the cloud automagically.
   @override
   void deserialize(map) {
     throw const ShouldNotCall(
         'You should not use this function with a Database List Provided.');
-  }
-
-  /// The path to the stored data inside the database.
-  final String pathToData;
-
-  /// The path to the list of available ids inside the database.
-  String _pathToAvailableDataIds;
-  // ignore: unnecessary_getters_setters
-  String get pathToAvailableDataIds => _pathToAvailableDataIds;
-  set pathToAvailableDataIds(String newPath) {
-    _pathToAvailableDataIds = newPath;
   }
 }
